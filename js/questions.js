09b44cf40b1868168ef7652d56648d45717d74dd
Mohammed Furqan <84570810+mdfurqan08@users.noreply.github.com>
2021-12-20 17:35:49 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,117 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Who Invented  PI π ?",
+    answer: "William Jones",
+    options: [
+      "William Jones",
+      "Hero of Alexandria",
+      "Aryabhata",
+      "Isaac Newton"
+    ]
+  },
+    {
+    numb: 2,
+    question: "If 5x-4 = 26,What does x equal?",
+    answer: "6",
+    options: [
+      "5",
+      "6",
+      "9",
+      "8",
+    ]
+  },
+    {
+    numb: 3,
+    question: "A simple closed curve made up of only _____________ is called a polygon ?",
+    answer: "line segments",
+    options: [
+      "curves",
+      "line segments",
+      "lines",
+      "closed curves",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the Father of Mathematics?",
+    answer: "Archimedes",
+    options: [
+      "Srinivasa Ramanujan",
+      "Pythagoras",
+      "Archimedes",
+      "Leonhard Euler"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who invented the algebra?",
+    answer: "Muhammad ibn Musa al-Khwarizmi",
+    options: [
+      "Albert Einstein",
+      "Muhammad ibn Musa al-Khwarizmi",
+      "David Hilbert",
+      "Ada Lovelace"
+    ]
+  },
+  
+
+   {
+    numb: 6,
+    question: "Who invented e=mc2 ?",
+    answer: "Albert Einstein",
+    options: [
+      "Euclid",
+      "David Hilbert",
+      "Archimedes",
+      "Albert Einstein"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "who invented number 0 ?",
+    answer: "Brahmagupta",
+    options: [
+      "Bhāskara II",
+      "Brahmagupta",
+      "Carl Friedrich Gauss",
+      "C. P. Ramanujam"
+    ]
+  },
+  {
+    numb: 8,
+    question: " All the angles of a regular polygon are of ________________.?",
+    answer: "equal measure",
+    options: [
+      "90°",
+      "60°",
+      "equal measure",
+      "equal length",
+    ]
+  }, 
+  {
+    numb: 9,
+    question: "The area of a square of side a is ?",
+    answer: "a²",
+    options: [
+      "a",
+     "a²",
+    " 2a",
+     "4a",
+    ]
+  },
+
+  {
+    numb: 10,
+    question: " 1 cm³ = ?",
+    answer: " 0.000001 m³",
+    options: [
+      " 0.000001 m³",
+      " 0.01 m³",
+      " 0.1 m³",
+      " 1000 m³",
+    ]
+  },
+];
